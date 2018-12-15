c748097987f2f2bd0de40fc7189e7910345b97ca
Brewington <212598938@logon.ds.ge.com>
2018-12-14 20:33:45 -0500

    Updated

--- app/data/employees.js
+++ app/data/employees.js
@@ -8,19 +8,19 @@ let employees = [
     {
         name: "Michelle",
         photo:"https://www.gannett-cdn.com/presto/2018/12/07/USAT/389be86b-cf2a-4a19-a0bb-03b9cb6e045d-Becoming_book_jacket_flat_Miller_Mobley.jpg?crop=1874,1054,x0,y626&width=3200&height=1680&fit=bounds",
-        scores:[1,3,3,1,1,2,1,2,1,1]
+        scores:[2,2,2,2,2,2,2,2,2,2]
     },
 
     {
         name: "Robbie",
         photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Robbie_Williams.jpg/220px-Robbie_Williams.jpg",
-        scores:[1,1,3,3,3,1,3,1,1,1]
+        scores:[3,3,3,3,3,3,3,3,3,3]
     },
 
     {
         name: "Poncho",
         photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/220px-President_Barack_Obama.jpg",
-        scores:[1,1,1,3,1,1,2,1,1,1]
+        scores:[1,2,3,3,2,1,1,2,3,1]
     },
 
 
