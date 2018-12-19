c2b364d2f58e92dff1b11af091a3e18a43e09825
Brewington <212598938@logon.ds.ge.com>
2018-12-19 11:28:10 -0500

    Updated Final

--- app/Routes/api-routes.js
+++ app/Routes/api-routes.js
@@ -6,10 +6,30 @@ module.exports = function(app){
 
 
    app.post('/api/employee', (req, res)=> {
-       console.log(req.body)
+        const newEmployee = req.body;
+        console.log(newEmployee)
+        let lowestDiff = null;
+        let bestMatch;
+        for( var i = 0; i < employees.length; i++){
+            const currentEmployee = employees[i];
+            let totalDiff =0;
+            for(var j = 0; j < currentEmployee.scores.length; j++){
+                const diff = parseInt(currentEmployee.scores[j]) - parseInt(newEmployee.scores[j]);
+                totalDiff += Math.abs(diff);
+            }
+            console.log(totalDiff, "This is the totoal diff")
+            console.log(currentEmployee, "This is the current employee")
+            if(totalDiff < lowestDiff || lowestDiff == null){
+                lowestDiff = totalDiff;
+                bestMatch = currentEmployee;
+
+            }
+        }
+        console.log(bestMatch, "Thsiis the bet match")
+
        employees.push(req.body);
-       console.log(employees)
-       res.send(employees[3]);
+       
+       res.send(bestMatch);
    });
 
    app.get('/api/employees', (req, res)=> {
